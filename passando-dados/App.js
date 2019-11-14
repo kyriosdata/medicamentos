c9cbdf6bca1e8fa6f763f4ccdee070f53b58a8ee
kyriosdata <fabio@inf.ufg.br>
2019-11-14 15:09:26 -0300

    configura git

--- passando-dados/App.js
+++ passando-dados/App.js
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import { useScreens } from 'react-native-screens';
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import { useScreens } from "react-native-screens";
 
 // Eficiência apenas (recomendado)
 useScreens();
 
-import SplashScreen from './SplashScreen';
-import Navegador from './Navegador';
+import SplashScreen from "./SplashScreen";
+import Navegador from "./Navegador";
 
 const fetchFonts = () => {
   Font.loadAsync({
-    'open-sans-light': require('./assets/OpenSans-Light.ttf'),
-    'open-sans-regular': require('./assets/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/OpenSans-Bold.ttf'),
+    "open-sans-light": require("./assets/OpenSans-Light.ttf"),
+    "open-sans-regular": require("./assets/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/OpenSans-Bold.ttf")
   });
 };
 
-export default function Nova(props) {
+export default function App(props) {
   const [autorizado, defineAutorizado] = useState(false);
- const [fontNotLoaded, setFontNotLoaded] = useState(true);
+  const [fontNotLoaded, setFontNotLoaded] = useState(true);
 
   const concluido = () => {
     defineAutorizado(true);
   };
-   
+
   if (fontNotLoaded) {
-    console.log('carregando fontes...');
+    console.log("carregando fontes...");
     return (
       <AppLoading
         startAsync={fetchFonts}
