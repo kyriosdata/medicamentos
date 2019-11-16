e7380c095f1624be37102739fca5d70caff399b3
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-11-16 19:27:08 -0300

    acrescenta amplify api

--- secure-s3/App.js
+++ secure-s3/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Storage } from "aws-amplify";
 
 export default function App() {
+  const [inicializado, setInicializado] = useState(false);
+  const [medicamentos, setMedicamentos] = useState(null);
+
+  if (inicializado) {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+
+  Storage.get("dados.json")
+    .then(data => setMedicamentos(data))
+    .then(x => setInicializado(true))
+    .catch(err => console.log("ocorreu um erro"));
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <View><Text>Loading...</Text></View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
