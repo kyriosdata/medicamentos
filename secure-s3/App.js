15b9f401d6f55b0833f7dd98782de939588696b2
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-11-16 21:59:18 -0300

    parcial

--- secure-s3/App.js
+++ secure-s3/App.js
@@ -1,6 +1,33 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Storage } from "aws-amplify";
+import Amplify, { Storage } from "aws-amplify";
+import axios from "axios";
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: "us-east-1:187ac862-fb82-45e9-bad0-5fe6468278f3",
+    region: "us-east-1"
+  },
+  Storage: {
+    AWSS3: {
+      bucket: "com.github.kyriosdata.medicamentos"
+    }
+  }
+});
+
+const trata = response => {
+  console.log("recebendo...");
+  console.log(response.data);
+};
+
+const error = error => console.error(error);
+
+function getRemoteData(url) {
+  return axios
+    .get(url)
+    .then(trata)
+    .catch(error);
+}
 
 export default function App() {
   const [inicializado, setInicializado] = useState(false);
@@ -15,13 +42,18 @@ export default function App() {
   }
 
   Storage.get("dados.json")
-    .then(data => setMedicamentos(data))
+    .then(url => {
+      console.log(url);
+      getRemoteData(url);
+    })
     .then(x => setInicializado(true))
     .catch(err => console.log("ocorreu um erro"));
-  
+
   return (
-    <View><Text>Loading...</Text></View>
-  )
+    <View>
+      <Text>Loading...</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
